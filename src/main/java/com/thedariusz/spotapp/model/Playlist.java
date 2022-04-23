45ab75a3194b34ba264a00f02b0af48e66cdb7a6
Dariusz Szczepański <thedariusz@gmail.com>
2022-04-23 20:30:24 +0200

    Playlist model was updated

--- src/main/java/com/thedariusz/spotapp/model/Playlist.java
+++ src/main/java/com/thedariusz/spotapp/model/Playlist.java
@@ -1,8 +1,20 @@
 package com.thedariusz.spotapp.model;
 
+import java.util.List;
+
 public record Playlist(
-        String id,
-        String name,
-        String description
+        String href,
+        List<Item> items,
+        int limit,
+        String next,
+        String previous,
+        int total
 ) {
+    private record Item(
+            String description,
+            String href,
+            String id,
+            String name
+    ) {
+    }
 }
